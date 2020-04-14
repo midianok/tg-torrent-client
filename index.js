9be394ce0792dbdc0d00dc31e50b9a7e164e7d02
IlyaGusev <midian.ip@gmail.com>
2020-04-14 23:07:16 +0300

    Magnet link validation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const WebTorrent = require('webtorrent');
 const Telegraf = require('telegraf');
+const magnetUri = require('magnet-uri');
 
 const client = new WebTorrent();
 const bot = new Telegraf(process.env.BOT_TOKEN);
@@ -8,6 +9,11 @@ bot.start((ctx) => ctx.reply('Send me magnet url'));
 bot.help((ctx) => ctx.reply('Send me magnet url'));
 
 bot.on('text', ctx => {
+    const infoHash =  magnetUri.decode(ctx.message.text).infoHash;
+    if (!infoHash) {
+        ctx.reply('It\'s not a magnet link');
+        return;
+    }
     const magnet = ctx.message.text;
     client.on('error', (err) => ctx.reply(err));
     client.add(magnet, { path: `${__dirname}/torrents/${ctx.chat.id}`}, torrent => {
